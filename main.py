af9b9f96fc80ba7eede56d1863679fcd75f6aa0f
Deccan Maniam <dev.oofsauce@gmail.com>
2026-06-19 18:24:04 -0400

    ui changes and dependencies

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 SP = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    scope="user-read-currently-playing user-library-read",
+    scope="user-read-currently-playing",
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri=REDIRECT_URI
@@ -54,31 +54,32 @@ class ImagePixel(Label):
     def watch_color(self, new_color) -> None:
         # with MUTEX:
             # self.styles.color = f"rgb({new_color[0]},{new_color[1]},{new_color[2]})" if SONG_DATA["playing"] else "auto"
+        self.styles.background = "rgb(0,0,0)"
         self.styles.color = f"rgb({new_color[0]},{new_color[1]},{new_color[2]})"
-        self.update("\u2588")
+        self.update("\u2593")
 
 class Song(Label):
-    song = reactive("No song playing.")
+    song = reactive("No song playing")
 
     def on_mount(self) -> None:
         self.set_interval(1, self.update_song)
 
     def update_song(self) -> None:
         with MUTEX:
-            self.song = SONG_DATA["name"] if SONG_DATA["playing"] else "No song playing."
+            self.song = SONG_DATA["name"] if SONG_DATA["playing"] else "No song playing"
 
     def watch_song(self, new_song) -> None:
         self.update(f"[bold]{new_song}[/]")
 
 class Artist(Label):
-    artist = reactive("")
+    artist = reactive("...")
 
     def on_mount(self) -> None:
         self.set_interval(1, self.update_artist)
 
     def update_artist(self) -> None:
         with MUTEX:
-            self.artist = SONG_DATA["artist"] if SONG_DATA["playing"] else ""
+            self.artist = SONG_DATA["artist"] if SONG_DATA["playing"] else "..."
 
     def watch_artist(self, new_artist) -> None:
         self.update(new_artist)
@@ -94,10 +95,10 @@ class Progress(Label):
             self.progress = SONG_DATA["progressPercentage"] if SONG_DATA["playing"] else 0
 
     def watch_progress(self, new_progress) -> None:
-        self.update(f"[{''.join(['\u2588' if new_progress > i/(SIZE*2-2) else ' ' for i in range(SIZE*2-2)])}]")
+        self.update(f"{''.join(['\u2588' if new_progress > i/(SIZE*2) else '\u2591' for i in range(SIZE*2)])}")
 
 class MusicApp(App):
-    TITLE = "SPOTIFY"
+    TITLE = "WALKMAN"
     BINDINGS = [("`", "toggle_dark", "dark mode")]
     CSS = f"""
     Screen {{
@@ -143,36 +144,40 @@ class MusicApp(App):
             yield Song(classes="songdata")
             yield Artist(classes="songdata")
             yield Progress(id="progress")
+            # no pause indicator
+            # no controls
 
     def action_toggle_dark(self) -> None:
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
 
+# this breaks after it runs for too long... why?
 def get_spotify():
     while(True):
-        # this breaks if (assumedly) any of these values are None
         results = SP.current_user_playing_track()
-        with MUTEX:
-            if(results["is_playing"]):
-                    SONG_DATA["playing"] = True
-                    SONG_DATA["name"] = results["item"]["name"]
-                    artist_string = ", ".join([artist["name"] for artist in results["item"]["artists"]])
-                    SONG_DATA["artist"] = artist_string if len(artist_string) < SIZE*2 else f"{artist_string[0:SIZE_2-3]}..."
-                    SONG_DATA["progressPercentage"] = results["progress_ms"] / results["item"]["duration_ms"]
-            else:
-                    SONG_DATA["playing"] = False
 
-        url = results["item"]["album"]["images"][0]["url"]
-        response = requests.get(url)
-        img = Image.open(io.BytesIO(response.content))
-        rgb = img.convert("RGB")
-        small = rgb.resize((SIZE, SIZE))
+        with MUTEX:
+            if(results):
+                SONG_DATA["playing"] = True
+                SONG_DATA["name"] = results["item"]["name"] or "[SONG]"
+                artist_string = ", ".join([artist["name"] for artist in results["item"]["artists"]]) or "[ARTIST]"
+                SONG_DATA["artist"] = artist_string if len(artist_string) < SIZE*2 else f"{artist_string[0:SIZE*2-3]}..."
+                SONG_DATA["progressPercentage"] = results["progress_ms"] / results["item"]["duration_ms"]
+            else:
+                SONG_DATA["playing"] = False
+
+        if results and len(results["item"]["album"]["images"]) > 0:
+            url = results["item"]["album"]["images"][-1]["url"]
+            response = requests.get(url)
+            img = Image.open(io.BytesIO(response.content))
+            rgb = img.convert("RGB")
+            small = rgb.resize((SIZE, SIZE), resample=Image.Resampling.HAMMING)
 
         with MUTEX:
             for i in range(SIZE):
                 for j in range(SIZE):
-                    IMG_COLORS[i][j] = small.getpixel((i, j))
+                    IMG_COLORS[i][j] = small.getpixel((i, j)) if results and len(results["item"]["album"]["images"]) > 0 else (0, 0, 0)
         time.sleep(1)
 
 def main():
