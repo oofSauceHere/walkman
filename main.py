dc542e14c9ca6741bc934b04476ccfa95cadbf16
Deccan Maniam <dev.oofsauce@gmail.com>
2026-06-19 01:28:00 -0400

    now updates live

--- main.py
+++ main.py
@@ -1,26 +1,100 @@
-# REMEMBER TO GIT PULL!!!
-
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Label, Static
 from textual.containers import Container, Horizontal, Vertical
+from textual.reactive import reactive
 from PIL import Image
 from functools import reduce
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import requests
 import io
+from dotenv import load_dotenv
+import os
+from threading import Thread, Lock
+import time
+
+load_dotenv()
 
 SIZE = 20
-IMG_COLORS = [[(255, 255, 255)]*SIZE for i in range(SIZE)]
+CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+SP = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    scope="user-read-currently-playing user-library-read",
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI
+))
 
-SP = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-currently-playing user-library-read"))
+MUTEX = Lock()
+IMG_COLORS = [[(0, 0, 0)]*SIZE for i in range(SIZE)]
 SONG_DATA = {
-    "playing": "false",
-    "name": None,
-    "album": None,
-    "artist": None,
+    "playing": False,
+    "name": "",
+    "album": "",
+    "artist": "",
     "progressPercentage": 0
 }
+
+class ImagePixel(Label):
+    color = reactive((0, 0, 0))
+
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+        super().__init__()
+
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_pixel)
+
+    def update_pixel(self) -> None:
+        with MUTEX:
+            self.color = IMG_COLORS[self.x][self.y] if SONG_DATA["playing"] else (0, 0, 0)
+
+    def watch_color(self, new_color) -> None:
+        # with MUTEX:
+            # self.styles.color = f"rgb({new_color[0]},{new_color[1]},{new_color[2]})" if SONG_DATA["playing"] else "auto"
+        self.styles.color = f"rgb({new_color[0]},{new_color[1]},{new_color[2]})"
+        self.update("\u2588")
+
+class Song(Label):
+    song = reactive("No song playing.")
+
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_song)
+
+    def update_song(self) -> None:
+        with MUTEX:
+            self.song = SONG_DATA["name"] if SONG_DATA["playing"] else "No song playing."
+
+    def watch_song(self, new_song) -> None:
+        self.update(f"[bold]{new_song}[/]")
+
+class Artist(Label):
+    artist = reactive("")
+
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_artist)
+
+    def update_artist(self) -> None:
+        with MUTEX:
+            self.artist = SONG_DATA["artist"] if SONG_DATA["playing"] else ""
+
+    def watch_artist(self, new_artist) -> None:
+        self.update(new_artist)
+
+class Progress(Label):
+    progress = reactive(0)
+
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_progress)
+
+    def update_progress(self) -> None:
+        with MUTEX:
+            self.progress = SONG_DATA["progressPercentage"] if SONG_DATA["playing"] else 0
+
+    def watch_progress(self, new_progress) -> None:
+        self.update(f"[{''.join(['\u2588' if new_progress > i/(SIZE*2-2) else ' ' for i in range(SIZE*2-2)])}]")
 
 class MusicApp(App):
     TITLE = "SPOTIFY"
@@ -38,66 +112,77 @@ class MusicApp(App):
     Vertical {{
         height: {SIZE};
         width: {SIZE*2};
+        margin-bottom: 1;
     }}
 
     Label {{
         margin: 0;
     }}
 
-    #songdata {{
+    .songdata {{
         width: {SIZE*2};
-        margin: 1;
         text-align: center;
     }}
 
-    #progreess {{
+    #progress {{
         width: {SIZE*2};
+        margin-top: 1;
     }}
     """
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        # perhaps should be grouped into one "Image" class
         with Container():
             with Vertical():
                 for i in range(SIZE):
                     with Horizontal():
                         for j in range(SIZE*2):
-                            color = IMG_COLORS[j//2][i]
-                            color_label = Label("\u2588")
-                            color_label.styles.color = f"rgb({color[0]},{color[1]},{color[2]})"
-                            yield color_label
-            yield Static(f"[bold]{SONG_DATA['name']}[/]\n{SONG_DATA['artist']}", id="songdata")
-            yield Static(f"[{"".join(["\u2588" if SONG_DATA['progressPercentage'] >= i/(SIZE*2-2) else " " for i in range(SIZE*2-2)])}]", id="progress")
+                            yield ImagePixel(j//2, i)
+            yield Song(classes="songdata")
+            yield Artist(classes="songdata")
+            yield Progress(id="progress")
 
     def action_toggle_dark(self) -> None:
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
 
+def get_spotify():
+    while(True):
+        # this breaks if (assumedly) any of these values are None
+        results = SP.current_user_playing_track()
+        with MUTEX:
+            if(results["is_playing"]):
+                    SONG_DATA["playing"] = True
+                    SONG_DATA["name"] = results["item"]["name"]
+                    artist_string = ", ".join([artist["name"] for artist in results["item"]["artists"]])
+                    SONG_DATA["artist"] = artist_string if len(artist_string) < SIZE*2 else f"{artist_string[0:SIZE_2-3]}..."
+                    SONG_DATA["progressPercentage"] = results["progress_ms"] / results["item"]["duration_ms"]
+            else:
+                    SONG_DATA["playing"] = False
+
+        url = results["item"]["album"]["images"][0]["url"]
+        response = requests.get(url)
+        img = Image.open(io.BytesIO(response.content))
+        rgb = img.convert("RGB")
+        small = rgb.resize((SIZE, SIZE))
+
+        with MUTEX:
+            for i in range(SIZE):
+                for j in range(SIZE):
+                    IMG_COLORS[i][j] = small.getpixel((i, j))
+        time.sleep(1)
+
 def main():
-
-
-    results = SP.current_user_playing_track()
-    if(results["is_playing"]):
-        SONG_DATA["playing"] = True
-        SONG_DATA["name"] = results["item"]["name"]
-        artist_string = ", ".join([artist["name"] for artist in results["item"]["artists"]])
-        SONG_DATA["artist"] = artist_string if len(artist_string) < SIZE*2 else f"{artist_string[0:SIZE_2-3]}..."
-        SONG_DATA["progressPercentage"] = results["progress_ms"] / results["item"]["duration_ms"]
-
-    url = results["item"]["album"]["images"][0]["url"]
-    response = requests.get(url)
-    img = Image.open(io.BytesIO(response.content))
-    rgb = img.convert("RGB")
-    small = rgb.resize((SIZE, SIZE))
-
-    for i in range(SIZE):
-        for j in range(SIZE):
-            IMG_COLORS[i][j] = small.getpixel((i, j))
+    t1 = Thread(target=get_spotify)
+    t1.start()
 
     app = MusicApp()
     app.run()
+
+    t1.join()
 
 if __name__ == "__main__":
     main()
